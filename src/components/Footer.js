36a9e42a3711f4fa71e945f6971e6151d6126a6b
Dreamcare-Admin <dreamcaredevelopers@gmail.com>
2024-12-28 20:59:24 +0530

    update image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -65,7 +65,7 @@ const Footer = () => {
       </div>
       <div>
         <img
-          src="/dhule-illustration.jpg"
+          src="/Dhule-illustration.jpg"
           alt="illustration"
           className="w-full h-auto object-contain"
           width={100}
